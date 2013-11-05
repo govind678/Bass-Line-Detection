3b38b718597b8f270bf22c8b383bccb81e2e35b2
Iman Mukherjee <iman.mukherjee@gmail.com>
2013-11-05 02:34:52 -0500

    Adding the AMDF files

---
+++ amdf.m
@@ -0,0 +1,15 @@
+%% AMDF
+
+function result = amdf(block)
+
+N = length(block);
+
+result = zeros(N-1,1);
+
+temp = [block; zeros(N-1,1)];
+
+for i = 10:N-1
+    result(i) = sum(abs(temp(i:i+N-1) - block(1:N)))/N;
+end
+
+end
---
+++ amdf_example.m
@@ -0,0 +1,29 @@
+[y,Fs] = wavread('sax.wav');
+
+filesize = length(y);
+
+blocksize = 2048;
+hopsize = 1024;
+
+period = zeros(hops, 1);
+freq = zeros(hops, 1);
+midi = zeros(hops, 1);
+hops = (filesize-blocksize)/hopsize-2;
+
+AMDF = zeros(blocksize-1,hops);
+
+for i = 0 : hops - 1
+    start = hopsize*i+1;
+    endpoint = start+blocksize - 1;
+
+    AMDF(:,i+1) = amdf(y(start : endpoint));
+    
+    [Val, period(i+1)] = min(AMDF(10:end,i+1));
+    
+    freq(i+1) = Fs/period(i+1);
+    
+    midi(i+1) = 69 + 12*log2(freq(i+1)/440);
+end
+
+plot(midi);
+% imagesc(AMDF);
